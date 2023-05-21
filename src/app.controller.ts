cb4031cb6657fd1ec9f150095468932764dd103e
Stavissky Artem <a.stavisskiy@atol.ru>
2023-05-21 15:37:31 +0300

    feature: main controllers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from "@nestjs/common";
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,14 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  findProc(input): any {
-    return this.appService.findProc(input);
+  getPartData(@Req() request): any {
+    const { part, input } = request.query;
+    return this.appService.getPartData({ part, input });
+  }
+
+  @Get('check')
+  getCheck(@Req() request): any {
+    const { id1, part1, id2, part2 } = request.query;
+    return this.appService.getCheck({ id1, part1, id2, part2 });
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,33 @@
 import { Injectable } from '@nestjs/common';
-import { HttpService } from '@nestjs/axios';
+import fetch from "node-fetch";
+
+function parseLinks(links: string): {} {
+  const rows = links.split(/\r\n|\r|\n/g);
+  const result = [];
+  rows.map(row=> {
+    const idIndex = row.indexOf("id=");
+    const modelIndex = row.indexOf("model=");
+    const modelEndIndex = row.indexOf("'>");
+    const id = row.substring(idIndex + 3, modelIndex - 1);
+    const name = row.substring(modelIndex + 6, modelEndIndex);
+    result.push({ id, name });
+  });
+  return result.slice(1, 11);
+}
 
 @Injectable()
 export class AppService {
-  constructor(private readonly httpService: HttpService) {}
+  async getPartData({ part, input }): Promise<{}> {
+    console.log(part, input);
+    // parts: MotherBoards, Processors, VideoCards, RAMs, Drives
+    const data = await fetch(`https://findhard.ru/checkcompatibility/GetAjaxSerp?parttype=${part}&input=${input}`);
+    return parseLinks(await data.text());
+  }
 
-  findProc(input): any {
-    const data = this.httpService.get(`https://findhard.ru/checkcompatibility/GetAjaxSerp?parttype=Processors&input=${input}`);
-    return data;
+  async getCheck({ id1, part1, id2, part2 }) {
+    // parts: MotherBoards, Processors, VideoCards, RAMs, Drives
+    const data = await fetch(`https://findhard.ru/checkcompatibility/GetCompareResult?partType=${part1}&id=${id1}&compPartType=${part2}&compPartId=${id2}`);
+    const dataText = await data.text();
+    return dataText.includes('Совместимы');
   }
 }
